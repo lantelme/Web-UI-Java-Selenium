754964bf9f82bd06c667fa732a6c51a18fd3285b
lantelme <albuzik97@gmail.com>
2021-11-15 18:05:17 +0700

    Lesson6

---
+++ src/main/java/Lesson6/BaseView.java
@@ -0,0 +1,21 @@
+package Lesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseView   {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+
+    @FindBy(xpath = "//body/div[contains(@class,'loader')]")
+    public WebElement loader;
+
+    public BaseView(WebDriver driver) {
+        this.driver = driver;
+        this.webDriverWait = new WebDriverWait(driver, 5);
+        PageFactory.initElements(driver, this);
+    }
+}
